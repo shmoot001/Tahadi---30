5d9eef74230f100472cfc5e92a88041cabf2e371
Ahmad Shammout <shmoot001@gmail.com>
2024-03-27 00:55:21 +0100

    NewUpdate

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="min-h-screen bg-blue-900 flex justify-center items-center">
+      <div className="grid grid-cols-2 gap-4">
+      <Link to='/mazataref'>
+          <button className="bg-blue-500 text-white py-3 px-6 rounded-lg shadow-md hover:bg-blue-600">
+            <a>ماذا تعرف</a>
+          </button>
+        </Link>
+        <Link to='/mazad'>
+          <button className="bg-blue-500 text-white py-3 px-10 rounded-lg shadow-md hover:bg-blue-600">
+            <a>مزاد</a>
+          </button>
+        </Link>
+        <Link to='/jaras'>
+          <button className="bg-blue-500 text-white py-3 px-10 rounded-lg shadow-md hover:bg-blue-600">
+            <a>جرس</a>
+          </button>
+        </Link>
+        <Link to='/altaawed'>
+          <button className="bg-blue-500 text-white py-3 px-6 rounded-lg shadow-md hover:bg-blue-600">
+            <a>التعويض</a>
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 }
