cc3c8e45c52a3b9f2d3d9370457431f936bc9ba5
Ahmad Shammout <shmoot001@gmail.com>
2024-03-27 01:12:18 +0100

    NewUpdate

--- app/src/pages/MazaTaraf.jsx
+++ app/src/pages/MazaTaraf.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 export default function MazaTaraf() {
   return (
-    <div>MazaTaraf</div>
-  )
+    <div className="min-h-screen bg-blue-900 flex flex-col justify-center items-center text-white">
+      <div className="text-center mb-8">
+        <h1 className="text-4xl">MazaTaraf</h1>
+      </div>
+      <button className="bg-blue-500 text-white py-3 px-6 rounded-lg shadow-md hover:bg-blue-600">
+        <a>سؤال جديد</a>
+      </button>
+    </div>
+  );
 }
