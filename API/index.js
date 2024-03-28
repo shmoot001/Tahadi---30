79840a89f5f0484b0b7511953844bf5b4d65325d
Ahmad Shammout <shmoot001@gmail.com>
2024-03-28 21:00:23 +0100

    Ändring för Deploy

--- API/index.js
+++ API/index.js
@@ -15,13 +15,18 @@ mongoose
         console.log(err);
     });
 
+const __dirname = path.resolve();
 const app = express();
 app.use(express.json());
 
 app.use('/api/mazataref/', MazatarefRouter);
 app.use('/api/mazad/', MazadRouter);
 
+app.use(express.static(path.join(__dirname, '/app/dist')));
 
+app.get('*', (req,res)=>{
+    res.sendFile(path.join(__dirname, 'app', 'dist', 'index'));
+});
 
 app.listen(3000, () =>{
     console.log('Server is running on port 3000');
